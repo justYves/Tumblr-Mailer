e09b7fa7b8d0bac75b0f4ec1ab8190573a1c1c7f
Yves Yuen <yves.j.yuen@gmail.com>
2015-06-06 14:24:27 -0400

    added ejs and tumblr API, completed README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Tumblr-Mailer
+
+<h2> OBJECTIVES </h2>
+
+Using the Tumblr blog you created in Part 1, you will create a node.js script file that will email a list of contacts your most recent blog posts.
+
+<h2> CONCEPTS </h2>
+
+This is your first project in Node where you will be using various libraries to accomplish your goals. When learning a new library, there is always a learning curve.
+
+This is a typical cycle of exploring a new API:
+
+<ul>
+	<li>play with the syntax</li>
+	<li>assume that something is messed up</li>
+	<li>read the documentation and see why the developer built it a certain way</li>
+	<li>assume that something is messed up</li>
+	<li>read the source code and realize that the developer was actually right all along</li>
+	<li>It can frustrating to use new libraries but don't worry - you will get through it! </li>
+	</ul>
+
+<h2> OVERVIEW </h2>
+Here's a high-level overview of tasks we'll be accomplishing:
+<ul>
+	<li>Create another JavaScript file that can pull data from your Tumblr Blog </li>
+	<li>Read in a CSV file of your friends' emails</li>
+	<li>Load the content from some time-period of posts from your Tumblr blog</li>
+	<li>Populate an email template with your blog content and mail merge it with your friends data</li>
+	<li>Send the email using the Mandrill API</li>
+	<li>By the end of the project, you should be able to run a JavaScript file that will send out an email newsletter to a list of friends and family in your CSV file.</li>
+</ul>
--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,24 +1,62 @@
-var fs = require('fs');
+var fs = require("fs");
+var ejs = require("ejs");
+var tumblr = require("tumblr.js");
+
+// Authenticate via OAuth
+var client = tumblr.createClient({
+  consumer_key: 'C1E1cyo3hX8PPO4AD6uehGVcEMwe9jsHCRLHnYbJJ6m7XwniRR',
+  consumer_secret: 'mAbkoVX0ZsJu4ri6CtO7O04xQen5HS1tN7IvLJWEZTCft5ssf2',
+  token: 'PFZhU4vwriHPNeYpDaFHuEwER9YtNi3G54prT2JdaxbMlyKma8',
+  token_secret: 'C7TVRP5jwNwKSIxLVUrwhi7xITzIsvigDTSZVQ0nDsRRP8ZPgP'
+});
+
+
 var csvFile = fs.readFileSync("friend_list.csv","utf8");
-var csv_data = []; 
+var emailTemplate = fs.readFileSync('email_template.html', 'utf-8');
 
 var csvParse = function(csvFile){
+	var csv_data = []; 
  //splitting the csvFile by line
  var csvFileArray = csvFile.split("\n");
 //save the first line of the csv into an the header Array
- var header = csvFileArray.shift().split(",");
+var header = csvFileArray.shift().split(",");
 
  //Loop through the friend's data
  for (var i = 0 ; i < csvFileArray.length; i++) {
  	var friendArray = csvFileArray[i].split(",");
  	var friend = {};
  	//Loop through the headers
- 		for(var j = 0; j < header.length; j++){
- 			friend[header[j]] = friendArray[j];
- 		}
- 		csv_data.push(friend);
+ 	for(var j = 0; j < header.length; j++){
+ 		friend[header[j]] = friendArray[j];
+ 	}
+ 	csv_data.push(friend);
  }
  return csv_data;
 };
 
-console.log(csvParse(csvFile));
+//console.log(csvParse(csvFile));
+
+var emailCreator = function(csvFile, emailTemplate){
+	var content = csvParse(csvFile); 
+	content.forEach(function(friend){
+		var personalizedEmail = "";
+		var customizedTemplate = ejs.render(emailTemplate, 
+			{ firstName: friend["firstName"],  
+			numMonthsSinceContact: friend["numMonthsSinceContact"]
+		});
+			console.log(customizedTemplate);
+	})
+};
+
+emailCreator(csvFile, emailTemplate);
+
+
+// Make the request
+client.userInfo(function (err, data) {
+    // ...
+});
+
+client.posts("yvesyuen.tumblr.com",function(err,blog){
+	console.log(blog);
+})
+
